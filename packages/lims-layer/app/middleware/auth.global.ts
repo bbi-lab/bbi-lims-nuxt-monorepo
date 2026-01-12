a19e1c3787aa865e44b3d181b32360abf17c853e
David Reinhart <davidrr@uw.edu>
2026-01-12 08:44:06 -0800

    Add auth middleware to lims-layer

---
+++ packages/lims-layer/app/middleware/auth.global.ts
@@ -0,0 +1,20 @@
+import _ from 'lodash'
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+    const { loggedIn, user, fetch } = useUserSession()
+
+    await fetch()
+    if (!_.get(user.value, 'isAdmin') && to.path.startsWith('/admin/')){
+      return navigateTo('/')
+    } else if (!loggedIn.value && !['/register', '/login'].includes(to.path)) {
+      // if not logged in and not currently headed to login page, redirect there
+      return navigateTo({path: '/login', query: {redirect: to.fullPath}})
+    } else if (loggedIn.value && ['/register', '/login'].includes(to.path)) {
+      // if already logged in and is headed to the login page, redirect to home
+      // to prevent infinite redirect
+      return navigateTo('/')
+    }
+
+    return
+  }
+)
