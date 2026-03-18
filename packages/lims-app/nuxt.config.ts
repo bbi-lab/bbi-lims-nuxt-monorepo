a5707ee2fa9c590f21ea2dd0dc20c9fb8b7fb4b9
David Reinhart <davidrr@uw.edu>
2026-03-18 14:02:38 -0700

    Add runtimeConfig to lims-app nuxt config file
    Needed because this property specificially is not merged from extended layer's nuxt configuration.

--- packages/lims-app/nuxt.config.ts
+++ packages/lims-app/nuxt.config.ts
@@ -3,4 +3,20 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
   extends: ['../lims-layer'],
+  runtimeConfig: {
+    dbHost: 'localhost',
+    dbPort: 5432,
+    dbDatabaseName: '',
+    dbUsername: '',
+    dbPassword: '',
+    dbSsl: false,
+    authJwtAccessTokenExpiresIn: '5m',
+    authJwtRefreshTokenExpiresIn: '60m',
+    authJwtAccessTokenSecret: 'access-token-secret-base64',
+    authJwtRefreshTokenSecret: 'refresh-token-secret-base64',
+    public: {
+      appName: 'LIMS app',
+      apiBase: '/api',
+    }
+  },
 })
