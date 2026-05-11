3157407432ec186b9d0ded4545e4a0022979cbc5
David Reinhart <davidrr@uw.edu>
2026-05-11 15:40:08 -0700

    Add labelseq-lims-app layer using lims-app as template
    Add drizzle-orm/zod to vite optimized dependencies

---
+++ packages/labelseq-lims-app/drizzle.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'drizzle-kit'
+import process from 'node:process'
+
+export default defineConfig({
+  schema: [
+    './shared/db/schema/*',
+    '../lims-layer/server/db/schema/*',
+    '../lims-layer/shared/db/schema/*'
+  ],
+  out: './server/db/migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    host: process.env.NUXT_DB_HOST || 'localhost',
+    port: Number(process.env.NUXT_DB_PORT || '5432'),
+    user: process.env.NUXT_DB_USER || 'postgres',
+    password: process.env.NUXT_DB_PASSWORD || 'postgres',
+    database: process.env.NUXT_DB_DATABASE_NAME || '',
+    ssl: 'require'
+  }
+})
--- packages/labelseq-lims-app/nuxt.config.ts
+++ packages/labelseq-lims-app/nuxt.config.ts
@@ -37,7 +37,7 @@ export default defineNuxtConfig({
     authJwtAccessTokenSecret: '',
     authJwtRefreshTokenSecret: '',
     public: {
-      appName: 'LIMS app',
+      appName: 'LABELseq LIMS',
       apiBase: '/api',
     }
   },
