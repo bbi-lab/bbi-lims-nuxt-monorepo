c6f95b1b81147bb01795f0fa2df6af570019ca54
David Reinhart <davidrr@uw.edu>
2026-05-11 20:24:01 -0700

    Change reverse complement and restriction enzyme sequence columns to stored generated columns

--- packages/labelseq-lims-app/shared/db/schema/primers.ts
+++ packages/labelseq-lims-app/shared/db/schema/primers.ts
@@ -1,5 +1,5 @@
 import { check, pgEnum, pgTable, text, uuid, varchar } from 'drizzle-orm/pg-core'
-import { sql } from 'drizzle-orm/sql'
+import { SQL, sql } from 'drizzle-orm/sql'
 
 export const primerDirectionEnum = pgEnum('primer_directions', ['forward', 'reverse'])
 
@@ -7,9 +7,8 @@ export const retrieverPrimers = pgTable('retriever_primers', {
     id: uuid('id').primaryKey().defaultRandom().notNull(),
     name: varchar('name', { length: 255 }).notNull(),
     seq: text('seq').notNull(),
-    seq_revcomp: text('seq_revcomp').notNull(),
+    seqRevComp: text('seq_rev_comp').generatedAlwaysAs((): SQL => sql`reverse(translate(${retrieverPrimers.seq}, 'aAcCgGtT', 'tTgGcCaA'))`),
     direction: primerDirectionEnum('direction').notNull(),
 }, (t) => [
     check("seq_check", sql`${t.seq} ~* '^[actg]*$'`),
-    check("seq_revcomp_check", sql`${t.seq_revcomp} ~* '^[actg]*$'`),
 ])
--- packages/labelseq-lims-app/shared/db/schema/reagents.ts
+++ packages/labelseq-lims-app/shared/db/schema/reagents.ts
@@ -1,16 +1,13 @@
 import { check, pgTable, uuid, varchar } from 'drizzle-orm/pg-core'
-import { sql } from 'drizzle-orm/sql'
+import { SQL, sql } from 'drizzle-orm/sql'
 
 export const restrictionEnzymes = pgTable('restriction_enzymes', {
     id: uuid('id').primaryKey().defaultRandom().notNull(),
     name: varchar('name', { length: 10, enum: ['bsa1', 'sap1', 'paqc1'] }).notNull(),
-    seq: varchar('recognition_site', { length: 20 }).notNull(),
-    seq_revcomp: varchar('recognition_site_revcomp', { length: 20 }).notNull(),
-    seq_plusone: varchar('seq_plusone', { length: 20 }),
-    seq_plusone_revcomp: varchar('seq_plusone_revcomp', { length: 20 }),
+    recogSeqPlusOne: varchar('recog_seq_plusone', { length: 20 }),
+    recogSeq: varchar('recog_seq', { length: 20 }).generatedAlwaysAs((): SQL => sql`substring(${restrictionEnzymes.recogSeqPlusOne}, 1, length(${restrictionEnzymes.recogSeqPlusOne}) - 1)`),
+    recogSeqRevComp: varchar('recog_seq_rev_comp').generatedAlwaysAs((): SQL => sql`reverse(translate(substring(${restrictionEnzymes.recogSeqPlusOne}, 1, length(${restrictionEnzymes.recogSeqPlusOne}) - 1), 'aAcCgGtT', 'tTgGcCaA'))`),
+    recogSeqPlusOneRevComp: varchar('recog_seq_plus_one_rev_comp').generatedAlwaysAs((): SQL => sql`reverse(translate(${restrictionEnzymes.recogSeqPlusOne}, 'aAcCgGtT', 'tTgGcCaA'))`),
 }, (t) => [
-    check("seq_check", sql`${t.seq} ~* '^[actg]*$'`),
-    check("seq_revcomp_check", sql`${t.seq_revcomp} ~* '^[actg]*$'`),
-    check("seq_plusone_check", sql`${t.seq_plusone} ~* '^[actg]*$'`),
-    check("seq_plusone_revcomp_check", sql`${t.seq_plusone_revcomp} ~* '^[actg]*$'`),
+    check("recog_seq_plusone_check", sql`${t.recogSeqPlusOne} ~* '^[actg]*$'`),
 ])
