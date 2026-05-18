eefe079cc8415f2c335156e1e4989b687c68ff93
David Reinhart <davidrr@uw.edu>
2026-05-18 13:43:30 -0700

    (chore) conslidate db migrations prior to initial deployment

--- packages/labelseq-lims-app/server/db/migrations/20260518204026_giant_nuke/migration.sql
+++ packages/labelseq-lims-app/server/db/migrations/20260518204026_giant_nuke/migration.sql
@@ -16,25 +16,29 @@ CREATE TABLE "projects" (
 	"name" varchar(255) NOT NULL,
 	"description" varchar(500),
 	"restriction_enzyme_id" uuid,
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now()
+	"created_at" timestamp,
+	"updated_at" timestamp
 );
 --> statement-breakpoint
 CREATE TABLE "restriction_enzymes" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
 	"name" varchar(10) NOT NULL UNIQUE,
-	"recog_seq_plusone" varchar(20),
-	"recog_seq" varchar(20) GENERATED ALWAYS AS (substring("restriction_enzymes"."recog_seq_plusone", 1, length("restriction_enzymes"."recog_seq_plusone") - 1)) STORED,
-	"recog_seq_rev_comp" varchar GENERATED ALWAYS AS (reverse(translate(substring("restriction_enzymes"."recog_seq_plusone", 1, length("restriction_enzymes"."recog_seq_plusone") - 1), 'aAcCgGtT', 'tTgGcCaA'))) STORED,
-	"recog_seq_plus_one_rev_comp" varchar GENERATED ALWAYS AS (reverse(translate("restriction_enzymes"."recog_seq_plusone", 'aAcCgGtT', 'tTgGcCaA'))) STORED,
-	CONSTRAINT "recog_seq_plusone_check" CHECK ("recog_seq_plusone" ~* '^[actg]*$')
+	"recog_seq_plus_overhang" varchar(20),
+	"overhang_length" integer NOT NULL,
+	"recog_seq" varchar(20) GENERATED ALWAYS AS (substring("restriction_enzymes"."recog_seq_plus_overhang", 1, length("restriction_enzymes"."recog_seq_plus_overhang") - "restriction_enzymes"."overhang_length")) STORED,
+	"recog_seq_rev_comp" varchar GENERATED ALWAYS AS (reverse(translate(substring("restriction_enzymes"."recog_seq_plus_overhang", 1, length("restriction_enzymes"."recog_seq_plus_overhang") - "restriction_enzymes"."overhang_length"), 'aAcCgGtT', 'tTgGcCaA'))) STORED,
+	"recog_seq_plus_overhang_rev_comp" varchar GENERATED ALWAYS AS (reverse(translate("restriction_enzymes"."recog_seq_plus_overhang", 'aAcCgGtT', 'tTgGcCaA'))) STORED,
+	CONSTRAINT "recog_seq_plus_overhang_check" CHECK ("recog_seq_plus_overhang" ~* '^[actgn]*$')
 );
 --> statement-breakpoint
 CREATE TABLE "superblocks" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
 	"project_id" uuid NOT NULL,
+	"refseq_transcript_id" uuid,
 	"name" varchar(50) NOT NULL,
 	"description" varchar(255),
+	"start" integer,
+	"end" integer,
 	"seq" text,
 	CONSTRAINT "seq_check" CHECK ("seq" ~* '^[actg]*$')
 );
@@ -49,8 +53,16 @@ CREATE TABLE "tiles" (
 	"mutagenesis_end" integer,
 	"retriever_primer_forward_id" uuid,
 	"retriever_primer_reverse_id" uuid,
-	"superblock_first" boolean DEFAULT false NOT NULL,
-	"last" boolean DEFAULT false NOT NULL
+	"superblock_first" boolean DEFAULT false,
+	"superblock_last" boolean DEFAULT false
+);
+--> statement-breakpoint
+CREATE TABLE "refseq_transcripts" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	"transcript_id" varchar(50) NOT NULL UNIQUE,
+	"gene_id" uuid NOT NULL,
+	"seq" text,
+	CONSTRAINT "seq_check" CHECK ("seq" ~* '^[actg]*$')
 );
 --> statement-breakpoint
 CREATE TABLE "users"."password_reset_tokens" (
@@ -159,9 +171,11 @@ CREATE INDEX "symbol_idx" ON "genes" ("symbol");--> statement-breakpoint
 CREATE INDEX "gene_type_idx" ON "genes" ("gene_type");--> statement-breakpoint
 ALTER TABLE "projects" ADD CONSTRAINT "projects_restriction_enzyme_id_restriction_enzymes_id_fkey" FOREIGN KEY ("restriction_enzyme_id") REFERENCES "restriction_enzymes"("id");--> statement-breakpoint
 ALTER TABLE "superblocks" ADD CONSTRAINT "superblocks_project_id_projects_id_fkey" FOREIGN KEY ("project_id") REFERENCES "projects"("id");--> statement-breakpoint
+ALTER TABLE "superblocks" ADD CONSTRAINT "superblocks_refseq_transcript_id_refseq_transcripts_id_fkey" FOREIGN KEY ("refseq_transcript_id") REFERENCES "refseq_transcripts"("id");--> statement-breakpoint
 ALTER TABLE "tiles" ADD CONSTRAINT "tiles_superblock_id_superblocks_id_fkey" FOREIGN KEY ("superblock_id") REFERENCES "superblocks"("id");--> statement-breakpoint
 ALTER TABLE "tiles" ADD CONSTRAINT "tiles_retriever_primer_forward_id_retriever_primers_id_fkey" FOREIGN KEY ("retriever_primer_forward_id") REFERENCES "retriever_primers"("id");--> statement-breakpoint
 ALTER TABLE "tiles" ADD CONSTRAINT "tiles_retriever_primer_reverse_id_retriever_primers_id_fkey" FOREIGN KEY ("retriever_primer_reverse_id") REFERENCES "retriever_primers"("id");--> statement-breakpoint
+ALTER TABLE "refseq_transcripts" ADD CONSTRAINT "refseq_transcripts_gene_id_genes_id_fkey" FOREIGN KEY ("gene_id") REFERENCES "genes"("id");--> statement-breakpoint
 ALTER TABLE "users"."password_reset_tokens" ADD CONSTRAINT "password_reset_tokens_user_id_users_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"."users"("id") ON DELETE CASCADE;--> statement-breakpoint
 ALTER TABLE "users"."user_group_memberships" ADD CONSTRAINT "user_group_memberships_user_id_users_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"."users"("id");--> statement-breakpoint
 ALTER TABLE "users"."user_group_memberships" ADD CONSTRAINT "user_group_memberships_user_group_id_user_groups_id_fkey" FOREIGN KEY ("user_group_id") REFERENCES "users"."user_groups"("id");--> statement-breakpoint
@@ -172,6 +186,38 @@ ALTER TABLE "well_content_sources" ADD CONSTRAINT "well_content_sources_created_
 ALTER TABLE "well_contents" ADD CONSTRAINT "well_contents_well_id_wells_id_fkey" FOREIGN KEY ("well_id") REFERENCES "wells"("id");--> statement-breakpoint
 ALTER TABLE "well_contents" ADD CONSTRAINT "well_contents_wellable_id_wellables_id_fkey" FOREIGN KEY ("wellable_id") REFERENCES "wellables"("id");--> statement-breakpoint
 ALTER TABLE "wells" ADD CONSTRAINT "wells_plate_id_plates_id_fkey" FOREIGN KEY ("plate_id") REFERENCES "plates"("id");--> statement-breakpoint
+CREATE VIEW "view_tiles_with_sequences" AS (select *,
+    concat(retriever_primer_f_seq, bsa1_plusone_seq, bsa1_plusone_nterm_overhang_seq, superblock_nterm_restriction_enzyme_seq, tile_seq, superblock_cterm_restriction_enzyme_seq_rev_comp, bsa1_plusone_cterm_overhang_seq, bsa1_plusone_seq_rev_comp, retriever_primer_r_seq_rev_comp) as full_sequence
+    from (
+        select
+        "tiles"."id",
+        "tiles"."superblock_id",
+        "tiles"."tile_name",
+        "tiles"."tile_start",
+        "tiles"."tile_end",
+        "tiles"."mutagenesis_start",
+        "tiles"."mutagenesis_end",
+        "tiles"."retriever_primer_forward_id",
+        "tiles"."retriever_primer_reverse_id",
+        "tiles"."superblock_first",
+        "tiles"."superblock_last",
+        "tiles"."tile_end" - "tiles"."tile_start" + 1 as tile_length,
+        "tiles"."mutagenesis_end" - "tiles"."mutagenesis_start" + 1 as mutagenesis_length,
+        "retriever_primer_f"."seq" as retriever_primer_f_seq,
+        'GGTCTCT' as bsa1_plusone_seq,
+        case when "tiles"."superblock_first" then 'CGTC' end as bsa1_plusone_nterm_overhang_seq,
+        case when "tiles"."superblock_first" then "restriction_enzymes"."recog_seq_plus_overhang" end as superblock_nterm_restriction_enzyme_seq,
+        substring("superblocks"."seq" from "tiles"."tile_start" for "tiles"."tile_end" - "tiles"."tile_start" + 1) as tile_seq,
+        case when "tiles"."superblock_last" then "restriction_enzymes"."recog_seq_plus_overhang_rev_comp" end as superblock_cterm_restriction_enzyme_seq_rev_comp,
+        case when "tiles"."superblock_last" then 'GCAT' end as bsa1_plusone_cterm_overhang_seq,
+        'AGAGACC' as bsa1_plusone_seq_rev_comp,
+        "retriever_primer_r"."seq_rev_comp" as retriever_primer_r_seq_rev_comp
+        from "tiles"
+        join "superblocks" on "tiles"."superblock_id" = "superblocks"."id"
+        join "projects" on "superblocks"."project_id" = "projects"."id"
+        join "restriction_enzymes" on "projects"."restriction_enzyme_id" = "restriction_enzymes"."id"
+        join "retriever_primers" as retriever_primer_f on "tiles"."retriever_primer_forward_id" = "retriever_primer_f"."id"
+        join "retriever_primers" as retriever_primer_r on "tiles"."retriever_primer_reverse_id" = "retriever_primer_r"."id"));--> statement-breakpoint
 CREATE VIEW "view_plates_with_well_counts" AS (select
     "plates"."id",
     "plates"."name",
