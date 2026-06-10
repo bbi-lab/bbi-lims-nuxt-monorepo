33d7f3b85aeb69804f8a1756c93bb12bf2c603a1
David Reinhart <davidrr@uw.edu>
2026-06-09 20:03:52 -0700

    fix (labelseq-lims-app): Update Refseq transcript POST endpoint to handle multiple records
    - Fetches multiple transcript IDs with single NCBI API request
    - Validation of parsed values from XML response and link to gene record
    - Improved error messages

--- packages/labelseq-lims-app/server/api/refseq-transcripts.post.ts
+++ packages/labelseq-lims-app/server/api/refseq-transcripts.post.ts
@@ -3,6 +3,9 @@ import { schemas } from '#shared/db/zod/zodSchemas'
 import type { ZodObject } from 'zod'
 import { XMLParser } from 'fast-xml-parser'
 import { refseqTranscripts } from '~~/shared/db/schema/transcripts'
+import assert from 'node:assert'
+
+const _fetch = $fetch as any
 
 export default defineEventHandler(async (event) => {
     const db = useAppDrizzle()
@@ -14,36 +17,50 @@ export default defineEventHandler(async (event) => {
             return insertSchema.parse(record)
         }) as RecordValues[]
 
+        // fetch NCBI data for all transcript IDs in one request to avoid rate limit.
+        const transcriptIds = _.map(records, 'transcriptId').join(',')
+        const ncbiResponse = await _fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=sequences&id=${transcriptIds}&rettype=xml&retmode=xml`) as string
+        const parser = new XMLParser()
+        const jsonDoc = parser.parse(ncbiResponse)
+        assert(jsonDoc, 'Failed to parse NCBI response as XML')
+
         // fetch gene ID, sequence, AA sequence, and description from NCBI REST API
         for (const record of records) {
-            const ncbiResponse = await $fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=sequences&id=${record.transcriptId}&rettype=xml&retmode=xml`) as string
-            const parser = new XMLParser()
-            const jsonDoc = parser.parse(ncbiResponse)
+            const recordJsonDoc = _.find(_.get(jsonDoc, 'GBSet', []), (doc) => _.get(doc, 'GBSeq_accession-version') == record.transcriptId)
+            assert(recordJsonDoc, `No NCBI record found for transcript ${record.transcriptId}`)
 
-            record.description = _.get(jsonDoc, 'GBSet.GBSeq.GBSeq_definition')
+            try {
+                record.description = _.get(recordJsonDoc, 'GBSeq_definition')
 
-            // find the CDS entry in the feature table, get the location (which is in the format "123..456"), and use that to extract the CDS sequence from the full sequence. Also extract the AA sequence from the qualifiers of the CDS entry
-            const cdsEntry = _.find(_.get(jsonDoc, 'GBSet.GBSeq.GBSeq_feature-table.GBFeature', []), (entry) => _.get(entry, 'GBFeature_key') === 'CDS')
-            const [cdsFrom, cdsTo] = _.get(cdsEntry, 'GBFeature_location', '').split('..')
+                // find the CDS entry in the feature table, get the location (which is in the format "123..456"), and use that to extract the CDS sequence from the full sequence. Also extract the AA sequence from the qualifiers of the CDS entry
+                const cdsEntry = _.find(_.get(recordJsonDoc, 'GBSeq_feature-table.GBFeature', []), (entry) => _.get(entry, 'GBFeature_key') === 'CDS')
+                const [cdsFrom, cdsTo] = _.get(cdsEntry, 'GBFeature_location', '').split('..')
 
-            const fullSequence = _.get(jsonDoc, 'GBSet.GBSeq.GBSeq_sequence')
-            if (cdsFrom && cdsTo && fullSequence) {
-                record.cds = fullSequence.substring(parseInt(cdsFrom) - 1, parseInt(cdsTo)).toUpperCase()
+                const fullSequence = _.get(recordJsonDoc, 'GBSeq_sequence')
+                if (cdsFrom && cdsTo && fullSequence) {
+                    record.cds = fullSequence.substring(parseInt(cdsFrom) - 1, parseInt(cdsTo)).toUpperCase()
+                }
+                const cdsEntryQuals = _.get(cdsEntry, 'GBFeature_quals.GBQualifier', [])
+                record.aa = _.get(_.find(cdsEntryQuals, (qual) => _.get(qual, 'GBQualifier_name') === 'translation'), 'GBQualifier_value').toUpperCase()
+                const ncbiGeneId = _.get(_.find(cdsEntryQuals, (qual) => _.get(qual, 'GBQualifier_name') === 'db_xref' && _.get(qual, 'GBQualifier_value')?.startsWith('GeneID:')), 'GBQualifier_value').split(':')[1]
+                record.ncbiGeneId = ncbiGeneId
+            } catch (e) {
+                throw new Error(`Failed to parse NCBI response for transcript ${record.transcriptId}`)
             }
-            const cdsEntryQuals = _.get(cdsEntry, 'GBFeature_quals.GBQualifier', [])
-            record.aa = _.get(_.find(cdsEntryQuals, (qual) => _.get(qual, 'GBQualifier_name') === 'translation'), 'GBQualifier_value').toUpperCase()
 
-            const ncbiGeneId = _.get(_.find(cdsEntryQuals, (qual) => _.get(qual, 'GBQualifier_name') === 'db_xref' && _.get(qual, 'GBQualifier_value')?.startsWith('GeneID:')), 'GBQualifier_value').split(':')[1]
-            const geneRecord = await db.query.genes.findFirst({
-                where: {ncbiGeneId} as any
-            })
-            record.geneId = geneRecord?.id
-
-            if (!record.cds || !record.aa) {
-                throw new Error(`Failed to fetch CDS or AA sequence for transcript ${record.transcriptId}`)
-            } else if (!record.geneId) {
-                throw new Error(`Failed to find gene record for transcript ${record.transcriptId} with NCBI Gene ID ${ncbiGeneId}`)
+            try {
+                const geneRecord = await db.query.genes.findFirst({
+                    where: {ncbiGeneId: record.ncbiGeneId} as any
+                })
+                assert(geneRecord, `No gene record found for NCBI Gene ID ${record.ncbiGeneId} (transcript ${record.transcriptId})`)
+                _.set(record, 'geneId', geneRecord.id)
+                _.unset(record, 'ncbiGeneId')
+            } catch (e) {
+                throw new Error(`Database error while fetching gene record for transcript ${record.transcriptId} (NCBI Gene ID: ${record.ncbiGeneId})`)
             }
+
+            assert(record.cds, `CDS sequence not found for transcript ${record.transcriptId}`)
+            assert(record.aa, `AA sequence not found for transcript ${record.transcriptId}`)
         }
 
         const newRecords = await insertRecords(refseqTranscripts, records)
